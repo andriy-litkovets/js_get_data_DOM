86540dcf1d1e3edd7a7cae71d9f33e5a17592068
Andrew Litkovets <andriylao@gmail.com>
2023-02-24 10:19:56 +0200

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const peopleSpan = document.querySelectorAll('.population');
+const totalValue = document.querySelector('.total-population');
+const averageValue = document.querySelector('.average-population');
+
+const totalPopulation = [...peopleSpan]
+  .map(el => Number(el.innerText.split(',').join('')))
+  .reduce((sum, elem) => sum + elem, 0);
+
+const averagePopulation = totalPopulation / peopleSpan.length;
+
+totalValue.innerText = totalPopulation.toLocaleString('en-US');
+averageValue.innerText = averagePopulation.toLocaleString('en-US');
